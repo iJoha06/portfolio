3ae634ff6e21a1181df288fd48212661dd9aa8ed
iJoha06 <yohannavarro0601@hotmail.com>
2026-01-19 15:11:36 -0500

    update

--- src/app/components/content/content.ts
+++ src/app/components/content/content.ts
@@ -2,15 +2,13 @@ import { Component } from '@angular/core';
 import { Profile } from "../profile/profile";
 import { Experience } from "../experience/experience";
 import { Projects } from "../projects/projects";
-import { Testimonials } from "../testimonials/testimonials";
 import { Education } from "../education/education";
 import { Articles } from "../articles/articles";
-import { Suscribe } from "../suscribe/suscribe";
 import { Skills } from "../skills/skills";
 
 @Component({
   selector: 'app-content',
-  imports: [Profile, Experience, Projects, Testimonials, Education, Articles, Suscribe, Skills],
+  imports: [Profile, Experience, Projects, Education, Articles, Skills],
   templateUrl: './content.html',
   styleUrl: './content.css',
 })
