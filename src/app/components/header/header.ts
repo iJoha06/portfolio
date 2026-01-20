5494591379c7a9286f82a6489c99e78a652bf04f
iJoha06 <yohannavarro0601@hotmail.com>
2026-01-20 11:31:42 -0500

    Darkmode update

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Theme } from '../../services/theme';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
 export class Header {
+  private theme = inject(Theme);
+  currentTheme: string = 'auto';
 
+  ngOnInit(): void {
+    this.currentTheme = this.theme.getCurrentTheme();
+  }
+
+  changeTheme(theme: 'light' | 'dark' | 'auto') {
+    this.theme.setTheme(theme);
+    this.currentTheme = theme;
+    console.log('THIS IS YOUR MODE', this.currentTheme);
+  }
 }
